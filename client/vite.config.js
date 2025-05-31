be85812739c35341c98bccb9cecf851f22af1da0
Haniif <haniifsatriawardana91@gmail.com>
2025-06-01 03:26:04 +0700

    deploy cicd pipeline
    Signed-off-by: Haniif <haniifsatriawardana91@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
     server: {
       proxy: {
         "/service": {
-          target: "http://localhost:5000",
+          target: mode === "development"
+            ? "http://localhost:5000"
+            : "https://e2425-wads-l4ccg2-server-haniif.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
         },
       },
     },
